39e6560a2857228df214144a6bafb9d1e0396a53
Sephiwe <sephiwe.pula@gmail.com>
2026-03-16 21:58:10 +0200

    Create script.js with organized JavaScript code from DijiLetter.html

---
+++ script.js
@@ -0,0 +1,60 @@
+// Array that holds different types of flowers
+const flowers = [
+    { name: 'Rose', color: 'Red' },
+    { name: 'Tulip', color: 'Yellow' },
+    { name: 'Lily', color: 'White' },
+    { name: 'Daisy', color: 'Pink' }
+];
+
+// Options for greenery
+const greeneryOptions = [
+    'Ferns',
+    'Eucalyptus',
+    'Ivy',
+    'Pine'
+];
+
+// Function to render the flower picker
+function renderPicker() {
+    // Implementation to render the flower picker
+}
+
+// Function to toggle the selected flower
+function toggleFlower(flower) {
+    // Implementation to toggle selected flower
+}
+
+// Function to render the bouquet based on selected flowers
+function renderBouquet(selectedFlowers) {
+    // Implementation to render the bouquet
+}
+
+// Function to change greenery option
+function changeGreenery(option) {
+    // Implementation to change greenery
+}
+
+// Function to encode share data
+function encodeShareData(data) {
+    // Implementation to encode share data
+}
+
+// Function to generate bouquet
+function generateBouquet() {
+    // Implementation to generate bouquet
+}
+
+// Function to copy share link to clipboard
+function copyShareLink() {
+    // Implementation to copy share link
+}
+
+// Function to load from share data
+function loadFromShare(data) {
+    // Implementation to load bouquet from share data
+}
+
+// On window load, initialize the app
+window.onload = function() {
+    // Implementation for initialization
+};
